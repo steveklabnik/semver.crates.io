031b0b52bdc1dfe009e74302e116d4ac64d71361
steveklabnik <steve@steveklabnik.com>
2017-11-26 12:47:40 -0500

    basic matching function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
-#[no_mangle]
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+extern crate semver;
+
+use semver::Version;
+use semver::VersionReq;
+
+fn is_match(v: &Version, r: &VersionReq) -> bool {
+    r.matches(v)
 }
+
+#[cfg(test)]
+mod tests {
+    use semver::Version;
+    use semver::VersionReq;
+    use super::is_match;
+
+    #[test]
+    fn matches() {
+        let r = VersionReq::parse(">= 1.0.0").unwrap();
+        let v = Version::parse("1.0.0").unwrap();
+
+        assert!(is_match(&v, &r));
+    }
+
+    #[test]
+    fn does_not_match() {
+        let r = VersionReq::parse(">= 1.0.0").unwrap();
+        let v = Version::parse("0.1.0").unwrap();
+
+        assert!(!is_match(&v, &r));
+    }
+}
+
