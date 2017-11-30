40c4dc83f30687926b4da0b534e8d7d826ff6682
steveklabnik <steve@steveklabnik.com>
2017-11-30 15:13:07 -0500

    list versions

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
+extern crate reqwest;
 extern crate simple_server;
 
 use simple_server::{Server, Method, StatusCode};
+
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -10,10 +12,12 @@ fn main() {
 
     println!("Starting server on http://{}:{}", host, port);
     let server = Server::new(|request, mut response| {
-        println!("Request received. {} {}", request.method(), request.uri());
+        let method = request.method();
+        let uri = request.uri();
+        println!("Request received. {} {}", method, uri.path());
 
         // workaround for https://github.com/steveklabnik/simple-server/issues/88
-        if let (&Method::GET, "/") = (request.method(), request.uri().path()) {
+        if let (&Method::GET, "/") = (method, uri.path()) {
             let mut f = File::open("public/index.html")?;
 
             let mut source = Vec::new();
@@ -22,6 +26,21 @@ fn main() {
 
             return Ok(response.body(source)?);
         }
+
+        if method == &Method::GET && uri.path().starts_with("/versions") {
+            // /versions/ is 10 chars long
+            let crate_name = &uri.path()[10..];
+
+            // DONT DO THIS KIDS DEAR GOD
+            let mut resp = reqwest::get(&format!("https://crates.io/api/v1/crates/{}/versions", crate_name)).unwrap();
+
+            let mut content = Vec::new();
+            resp.read_to_end(&mut content);
+
+            response.header("Content-Type", "application/json");
+            return Ok(response.body(content)?);
+        }
+        
 
         response.status(StatusCode::NOT_FOUND);
         Ok(response.body("<h1>404</h1><p>Not found!<p>".as_bytes().to_vec())?)
